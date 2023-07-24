ae7dee54978bc669cab2ae401b1790e5a7cdf98a
nethen <72537484+nethen@users.noreply.github.com>
2023-07-24 06:06:10 -0700

    Updated font globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+
+const chiaroscuro = localFont({
+  src: [
+    {
+      path: '../fonts/chiaroscuro/ChiaroscuroSlant-Slant.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/chiaroscuro/ChiaroscuroSlant-Slant.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+})
+
+const helvetica = localFont({
+  src: [
+    {
+      path: '../fonts/helvetica/HelveticaNeue-Roman.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/helvetica/HelveticaNeue-BD.woff',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={helvetica.className}><Component {...pageProps} /> </main>
 }
