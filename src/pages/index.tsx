2c9bbdabeba6b0741e3409a171a47014bfd42628
nethen <72537484+nethen@users.noreply.github.com>
2023-07-28 12:42:34 -0700

    Added link to save date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,10 @@ export default function Home() {
       <Nav />
       <main className="min-h-screen flex flex-col justify-center px-4">
         <section className="flex flex-col lg:flex-row items-center lg: justify-center my-16 gap-8">
-          <p className={`uppercase text-center lg:translate-x-full`}>November 11 {viewport.width >= 1024 && <br/>}2023</p>
+          <Link 
+            className={`uppercase text-center lg:translate-x-full`}
+            href="http://www.google.com/calendar/event?action=TEMPLATE&text=TEDxSFU%202023:%20Unmask%20the%20Magic&dates=20231111T160000Z/20231112T020000Z&details=Event%20Details%20Here&location=777%20Homer%20St%2C%20Vancouver%2C%20BC"
+            target="_blank">November 11 {viewport.width >= 1024 && <br/>}2023</Link>
           <Logo />
           <Link className={`uppercase text-center lg:-translate-x-full`} href="https://goo.gl/maps/yx7ytZ2okDUuF33q7" target="_blank">The Centre for<br/>Performing Arts</Link>
         </section>
