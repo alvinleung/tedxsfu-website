2de341c677a254cb35eaf808d4b407733f823a7b
nethen <72537484+nethen@users.noreply.github.com>
2023-07-27 21:19:23 -0700

    Made home page

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useRouter } from "next/router"
 
 type Props = {};
 
 const Nav = (props: Props) => {
+  const path = useRouter();
   return (
     <nav className="block fixed left-0 right-0 top-0 h-8">
-      <img className="block h-16" src="./TEDxSFU_logo_black.svg" />
+      {path.pathname == "/" ? <img className="block h-16" src="./TEDxSFU_logo_white.svg" /> : <img className="block h-16" src="./TEDxSFU_logo_black.svg" />}
     </nav>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link"
 import Logo from "@/EmergeTextEffect/Logo";
 import { useMousePosition } from "@/hooks/useMousePosition";
+import Nav from "@/component/Nav";
 
 export default function Home() {
   const pos = useMousePosition();
   
   return (
+    <>
+      <Nav />
       <main className="min-h-screen flex flex-col justify-center px-4">
         <section className="flex flex-col lg:flex-row items-center lg: justify-center my-16 gap-8">
           <p className={`uppercase text-center lg:translate-x-full`}>November 11 2023</p>
@@ -13,6 +16,7 @@ export default function Home() {
           <Link className={`uppercase text-center lg:-translate-x-full`} href="https://goo.gl/maps/yx7ytZ2okDUuF33q7" target="_blank">The Centre for<br/>Performing Arts</Link>
         </section>
       </main>
+    </>
   );
 }
 
