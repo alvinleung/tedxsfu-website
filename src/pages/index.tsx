b16440dd9b37b19afbd8f1631d104b3f05b8ce49
nethen <72537484+nethen@users.noreply.github.com>
2023-07-27 21:23:47 -0700

    Responsive break on date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link"
 import Logo from "@/EmergeTextEffect/Logo";
-import { useMousePosition } from "@/hooks/useMousePosition";
+import { useWindowDimension } from "@/hooks/useWindowDimension";
 import Nav from "@/component/Nav";
 
 export default function Home() {
-  const pos = useMousePosition();
+  const viewport = useWindowDimension();
   
   return (
     <>
       <Nav />
       <main className="min-h-screen flex flex-col justify-center px-4">
         <section className="flex flex-col lg:flex-row items-center lg: justify-center my-16 gap-8">
-          <p className={`uppercase text-center lg:translate-x-full`}>November 11 2023</p>
+          <p className={`uppercase text-center lg:translate-x-full`}>November 11 {viewport.width >= 1024 && <br/>}2023</p>
           <Logo />
           <Link className={`uppercase text-center lg:-translate-x-full`} href="https://goo.gl/maps/yx7ytZ2okDUuF33q7" target="_blank">The Centre for<br/>Performing Arts</Link>
         </section>
