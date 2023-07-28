b9ac69e238ae88c6f6c1fad27294629033427087
nethen <72537484+nethen@users.noreply.github.com>
2023-07-27 21:15:28 -0700

    Styled svg placement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import TypeographyTest from "@/component/TypeographyTest";
-import Image from "next/image";
+import Link from "next/link"
+import Logo from "@/EmergeTextEffect/Logo";
+import { useMousePosition } from "@/hooks/useMousePosition";
 
 export default function Home() {
+  const pos = useMousePosition();
+  
   return (
-    <main className="bg-white text-black flex min-h-screen">
-      <TypeographyTest />
-    </main>
+      <main className="min-h-screen flex flex-col justify-center px-4">
+        <section className="flex flex-col lg:flex-row items-center lg: justify-center my-16 gap-8">
+          <p className={`uppercase text-center lg:translate-x-full`}>November 11 2023</p>
+          <Logo />
+          <Link className={`uppercase text-center lg:-translate-x-full`} href="https://goo.gl/maps/yx7ytZ2okDUuF33q7" target="_blank">The Centre for<br/>Performing Arts</Link>
+        </section>
+      </main>
   );
 }
+
