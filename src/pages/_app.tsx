be22188df7f416e141bbc5c28eef2de4d5dead6a
nethen <72537484+nethen@users.noreply.github.com>
2023-07-29 11:31:51 -0700

    Google analytics supposedly configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Cursor from "@/component/Cursor";
+import Script from "next/script";
 import { ScrollContainer } from "@/component/ScrollContainer";
 import { MousePosContextProvider } from "@/hooks/useMousePosition";
 import { WindowDimensionContextProvider } from "@/hooks/useWindowDimension";
@@ -43,6 +44,17 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* <Cursor /> */}
         <ScrollContainer>
           <main className={`${helvetica.className}`}>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=UA-83137117-1" />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'UA-83137117-1');
+            `}
+          </Script>
+
             <Component {...pageProps} />{" "}
           </main>
         </ScrollContainer>
