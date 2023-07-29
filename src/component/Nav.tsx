628c03309e09559969c0f30afecee490d67518b4
nethen <72537484+nethen@users.noreply.github.com>
2023-07-29 11:47:24 -0700

    Updated alt text

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = (props: Props) => {
   const path = useRouter();
   return (
     <nav className="block fixed left-0 right-0 top-0 h-8">
-      {path.pathname == "/" ? <img className="block h-16" src="./TEDxSFU_logo_white.svg" /> : <img className="block h-16" src="./TEDxSFU_logo_black.svg" />}
+      {path.pathname == "/" ? <img className="block h-16" src="./TEDxSFU_logo_white.svg" alt="TEDxSFU" /> : <img className="block h-16" src="./TEDxSFU_logo_black.svg" alt="TEDxSFU" />}
     </nav>
   );
 };
