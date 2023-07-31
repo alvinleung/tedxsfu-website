3b3a91e27970dc22d49dbef30f1232b340add491
nethen <72537484+nethen@users.noreply.github.com>
2023-07-30 18:50:06 -0700

    Updated logo alt text

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -8,9 +8,9 @@ const Nav = (props: Props) => {
   return (
     <nav className="block fixed left-0 right-0 top-0 h-8 z-50">
       {path.pathname == "/" ? (
-        <img className="block h-16 w-fit" src="./TEDxSFU_logo_white.svg" width="1168" height="438" />
+        <img className="block h-16 w-fit" src="./TEDxSFU_logo_white.svg" width="1168" height="438" alt="TEDxSFU"/>
       ) : (
-        <img className="block h-16 w-fit" src="./TEDxSFU_logo_black.svg" width="1168" height="438"/>
+        <img className="block h-16 w-fit" src="./TEDxSFU_logo_black.svg" width="1168" height="438" alt="TEDxSFU"/>
       )}
     </nav>
   );
