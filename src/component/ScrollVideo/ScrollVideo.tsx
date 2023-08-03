a80a99fd930020fbcad71ea5081b9d690d0758d9
Alvin Leung <alvinleung2009@gmail.com>
2023-08-03 12:20:20 -0700

    new scrubbing video approach

--- src/component/ScrollVideo/ScrollVideo.tsx
+++ src/component/ScrollVideo/ScrollVideo.tsx
@@ -20,14 +20,15 @@ const ScrollVideo = (props: Props) => {
 
   useEffect(() => {
     const scrollStartPosition = containerBound.top;
-    const scrollHeight =
-      containerBound.bottom - scrollStartPosition + windowDim.height;
+    const scrollHeight = containerBound.bottom - scrollStartPosition;
 
     const cancel = scrollY.on("change", (v) => {
       const timeProgress = (v - scrollStartPosition) / scrollHeight;
+
       const timeProgressClamped = clamp(0, 1, timeProgress);
 
-      videoFrame.current = timeProgressClamped * videoRef.current.duration;
+      videoFrame.current =
+        timeProgressClamped * Math.floor(videoRef.current.duration);
 
       if (timeProgress > 0 && timeProgress < 1) {
         setIsScrubbingVideo(true);
@@ -43,6 +44,8 @@ const ScrollVideo = (props: Props) => {
 
     let animFrame = 0;
     const updateFrame = () => {
+      // videoRef.current.fastSeek(videoFrame.current);
+      console.log(videoFrame.current);
       videoRef.current.currentTime = videoFrame.current;
       animFrame = requestAnimationFrame(updateFrame);
     };
@@ -58,7 +61,7 @@ const ScrollVideo = (props: Props) => {
         autobuffer="autobuffer"
         preload="preload"
         ref={videoRef}
-        src="./about/about-intro-video.mp4"
+        // src="./about/about-intro-video.mp4"
         loop
         muted
         // autoPlay
@@ -67,9 +70,13 @@ const ScrollVideo = (props: Props) => {
           zIndex: -1000,
         }}
       >
-        <source
+        {/* <source
           type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
           src="https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4"
+        ></source> */}
+        <source
+          type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+          src="./about/about-intro-video.mp4"
         ></source>
       </video>
     </>
