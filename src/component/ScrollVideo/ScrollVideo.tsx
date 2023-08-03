b56a4ba30215d28e9d262c706b90a50029da0870
Alvin Leung <alvinleung2009@gmail.com>
2023-08-02 19:49:18 -0700

    optimise scroll video

--- src/component/ScrollVideo/ScrollVideo.tsx
+++ src/component/ScrollVideo/ScrollVideo.tsx
@@ -1,4 +1,4 @@
-import React, { MutableRefObject, useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { useContainerScroll } from "../ScrollContainer/ScrollContainer";
 import { useStickyContainerBounds } from "../ScrollContainer/StickyContainer";
 import { useBoundingBox } from "@/hooks/useBoundingBox";
@@ -13,6 +13,8 @@ const ScrollVideo = (props: Props) => {
   // const videoRef = useRef() as MutableRefObject<HTMLVideoElement>;
   const containerBound = useStickyContainerBounds();
   const [videoRef, videoBounds] = useBoundingBox<HTMLVideoElement>([]);
+  const videoFrame = useRef(0);
+  const [isScrubbingVideo, setIsScrubbingVideo] = useState(false);
 
   useEffect(() => {
     const scrollStartPosition = containerBound.top;
@@ -22,11 +24,29 @@ const ScrollVideo = (props: Props) => {
     const cancel = scrollY.on("change", (v) => {
       const timeProgress = (v - scrollStartPosition) / scrollHeight;
       const timeProgressClamped = clamp(0, 1, timeProgress);
-      videoRef.current.currentTime =
-        timeProgressClamped * videoRef.current.duration;
+      videoFrame.current = timeProgressClamped * videoRef.current.duration;
+
+      if (timeProgress > 0 && timeProgress < 1) {
+        setIsScrubbingVideo(true);
+      } else {
+        setIsScrubbingVideo(false);
+      }
     });
     return () => cancel();
   }, [videoRef, videoBounds, containerBound]);
+
+  useEffect(() => {
+    if (!isScrubbingVideo) return;
+
+    let animFrame = 0;
+    const updateFrame = () => {
+      videoRef.current.currentTime = videoFrame.current;
+      animFrame = requestAnimationFrame(updateFrame);
+    };
+    animFrame = requestAnimationFrame(updateFrame);
+
+    return () => cancelAnimationFrame(animFrame);
+  }, [isScrubbingVideo]);
 
   return (
     <>
