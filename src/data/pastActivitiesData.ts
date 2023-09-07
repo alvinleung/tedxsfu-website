cb3b169cb2ac64a4fb78550217476de122785a6d
nethen <72537484+nethen@users.noreply.github.com>
2023-09-07 11:39:03 -0700

    More gallery updates

--- src/data/pastActivitiesData.ts
+++ src/data/pastActivitiesData.ts
@@ -31,7 +31,7 @@ const pastActivities = [
     date: "5/20",
     header: "TEDxPortland",
     description:
-      "We met up and learned how to scale up with the team behind the largest indoor TEDx event in North America.",
+      "We met up and learned how to scale up with the people behind the largest indoor TEDx event in North America.",
     media: [
       {
         src: "/about/activities/portland-0.jpg",
@@ -40,7 +40,12 @@ const pastActivities = [
       },
       {
         src: "/about/activities/portland-1.jpeg",
-        format: "square",
+        format: "portrait",
+        type: "image",
+      },
+      {
+        src: "/about/activities/portland-3.jpg",
+        format: "landscape",
         type: "image",
       },
       {
@@ -51,32 +56,45 @@ const pastActivities = [
     ],
   },
 
+  {
+    month: "jun",
+    day: "13",
+    date: "6/13",
+    header: "Growing the team",
+    description:
+      "With eager smiles and new faces, we found the people to hit the 1,800 seat mark.",
+    media: [
+      {
+        src: "/about/activities/earnest-0.jpg",
+        format: "portrait",
+        type: "image",
+      },
+      {
+        src: "/about/activities/earnest-1.jpg",
+        format: "portrait",
+        type: "image",
+      },
+     
+    ],
+  },
 
-  // {
-  //   month: "jun",
-  //   day: "20",
-  //   date: "6/20",
-  //   header: "Speaker sessions",
-  //   description:
-  //     "We met up with the people behind the largest indoor TEDx event in North America.",
-  //   media: [
-  //     {
-  //       src: "/about/activities/tedxsfu-live-2.jpg",
-  //       format: "landscape",
-  //       type: "image",
-  //     },
-  //     {
-  //       src: "/about/activities/tedxsfu-live-1.jpg",
-  //       format: "square",
-  //       type: "image",
-  //     },
-  //     {
-  //       src: "/about/activities/tedxsfu-live-3.jpg",
-  //       format: "landscape",
-  //       type: "image",
-  //     },
-  //   ],
-  // },
+
+  {
+    month: "jul",
+    day: "23",
+    date: "7/23",
+    header: "Speaker sessions",
+    description:
+      "In prep for November, we gathered with our speakers to build on their idea worth spreading.",
+    media: [
+      {
+        src: "/about/activities/speaker-1.jpg",
+        format: "landscape",
+        type: "image",
+      },
+
+    ],
+  },
 ];
 
 export { pastActivities };
