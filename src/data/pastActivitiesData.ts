b6cfcef769f52d702f5078685fbc81bea8ee86f5
nethen <72537484+nethen@users.noreply.github.com>
2023-09-07 11:21:15 -0700

    Gallery update

--- src/data/pastActivitiesData.ts
+++ src/data/pastActivitiesData.ts
@@ -24,56 +24,59 @@ const pastActivities = [
       },
     ],
   },
-  {
-    month: "may",
-    day: "7",
-    date: "5/7",
-    header: "TEDxSFU Live 2",
-    description:
-      "An initial venture in building physical experiences and truly understanding, the value of community.",
-    media: [
-      {
-        src: "/about/about-1.jpg",
-        format: "landscape",
-        type: "image",
-      },
-      {
-        src: "/about/about-2.jpg",
-        format: "square",
-        type: "image",
-      },
-      {
-        src: "/about/about-3.jpg",
-        format: "landscape",
-        type: "image",
-      },
-    ],
-  },
+
   {
     month: "may",
     day: "20",
     date: "5/20",
     header: "TEDxPortland",
     description:
-      "We met up with the people behind the largest indoor TEDx event in North America.",
+      "We met up and learned how to scale up with the team behind the largest indoor TEDx event in North America.",
     media: [
       {
-        src: "/about/activities/tedxsfu-live-2.jpg",
-        format: "landscape",
+        src: "/about/activities/portland-0.jpg",
+        format: "portrait",
         type: "image",
       },
       {
-        src: "/about/activities/tedxsfu-live-1.jpg",
+        src: "/about/activities/portland-1.jpeg",
         format: "square",
         type: "image",
       },
       {
-        src: "/about/activities/tedxsfu-live-3.jpg",
-        format: "landscape",
+        src: "/about/activities/portland-2.jpeg",
+        format: "portrait",
         type: "image",
       },
     ],
   },
+
+
+  // {
+  //   month: "jun",
+  //   day: "20",
+  //   date: "6/20",
+  //   header: "Speaker sessions",
+  //   description:
+  //     "We met up with the people behind the largest indoor TEDx event in North America.",
+  //   media: [
+  //     {
+  //       src: "/about/activities/tedxsfu-live-2.jpg",
+  //       format: "landscape",
+  //       type: "image",
+  //     },
+  //     {
+  //       src: "/about/activities/tedxsfu-live-1.jpg",
+  //       format: "square",
+  //       type: "image",
+  //     },
+  //     {
+  //       src: "/about/activities/tedxsfu-live-3.jpg",
+  //       format: "landscape",
+  //       type: "image",
+  //     },
+  //   ],
+  // },
 ];
 
 export { pastActivities };
